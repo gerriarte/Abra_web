9d0af244e213423b3b1a3e13f956a7f6237af00b
Ger Riarte <gerardoriarte@gmail.com>
2025-11-08 11:38:24 -0500

    feat: Vincular GTM ID directamente (GTM-56J4SKMV)

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,8 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const gtmId = process.env.NEXT_PUBLIC_GTM_ID;
+  // GTM ID - puede venir de variable de entorno o usar el valor por defecto
+  const gtmId = process.env.NEXT_PUBLIC_GTM_ID || 'GTM-56J4SKMV';
 
   return (
     <html lang="en">
@@ -31,33 +32,29 @@ export default function RootLayout({
         {/* Google Tag Manager (noscript) - Debe estar inmediatamente después del body */}
         <GoogleTagManagerNoscript gtmId={gtmId} />
         {/* Inicializar dataLayer antes de GTM */}
-        {gtmId && (
-          <Script
-            id="gtm-dataLayer-init"
-            strategy="beforeInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-              `,
-            }}
-          />
-        )}
+        <Script
+          id="gtm-dataLayer-init"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+            `,
+          }}
+        />
         {/* Google Tag Manager Script */}
-        {gtmId && (
-          <Script
-            id="google-tag-manager"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','${gtmId}');
-              `,
-            }}
-          />
-        )}
+        <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${gtmId}');
+            `,
+          }}
+        />
         {children}
         <GTMDebug />
       </body>
